a64027ebbb5473159631ce57ebafcd5d361d65a5
Olga Garmonenko <olga.garmonenko@intexsoft.by>
2022-04-06 10:15:06 +0300

    Refasctoring

---
+++ src/api/enums.ts
@@ -0,0 +1,10 @@
+export const enum HttpStatusCode  {
+    OK = 200,
+    SREVER_ERROR = 500
+}
+
+export const enum ACTION  {
+    GET_RECORDS = 'getRecords',
+    ADD_RECORD = 'addRecord',
+    REMOVE_RECORD = 'removeRecord',
+}
