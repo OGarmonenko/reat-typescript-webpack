f026686c16cf431ba95480bd78532819d75dd256
Olga Garmonenko <olga.garmonenko@intexsoft.by>
2022-04-14 13:58:53 +0300

    Refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
 import './App.css';
 import React from 'react';
-import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import MainPage from '@pages//main/MainPage';
 import CardPage from '@pages/card/CardPage';
-import constants from '@constants';
+import constants from "@constants";
 
-const App = ()=> {
-    return (
-        <BrowserRouter>
-             <Routes>
-                <Route path={constants.ROUTES.MAIN_PATH} element={ <MainPage/> } />
-                <Route path={constants.ROUTES.CARD_PATH + ':recordID'} element={ <CardPage /> } />
-             </Routes>
-        </BrowserRouter>
-    );
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path={constants.ROUTES.MAIN_PATH} element={<MainPage />} />
+        <Route path={constants.ROUTES.CARD_PATH + ':recordID'} element={<CardPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
