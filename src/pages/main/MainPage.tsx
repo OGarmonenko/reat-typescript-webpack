9cb2d423cd4cbd15f78be6aa4bb8e3b79beb8ef0
Olga Garmonenko <olga.garmonenko@intexsoft.by>
2022-04-14 13:55:14 +0300

    Refactoring

--- src/pages/main/MainPage.tsx
+++ src/pages/main/MainPage.tsx
@@ -8,11 +8,11 @@ import constants from '@constants';
 import { httpService } from '@api/httpService';
 import { storeService } from '@store/storeService';
 import { ACTION, HttpStatusCode } from '@api/enums';
-import Modal from "@components/custom/modal/Modal";
+import Modal from '@components/custom/modal/Modal';
 
 const MainPage: FC = () => {
-  const [records, setRecords] = useState<Record_Props[]>([])
-  const [isLoading, setIsLoading] = useState<boolean>(true)
+  const [records, setRecords] = useState<Record_Props[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
   const [needRefresh, setNeedRefresh] = useState<boolean>(false);
   const history = useNavigate();
