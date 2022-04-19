2debdf7700222d2c000f85659fb9bc81134cd2fa
Olga Garmonenko <olga.garmonenko@intexsoft.by>
2022-04-19 17:25:33 +0300

    Refactoring MappingModal.tsx

--- src/components/custom/modal/Modal.tsx
+++ src/components/custom/modal/Modal.tsx
@@ -2,13 +2,11 @@ import React, { FC } from 'react';
 import styles from './Modal.module.css';
 import { Modal_Props } from '@interfaces/interfaceModalProps';
 
-const Modal: FC<Modal_Props> = ({ header, content, footer, headerColor = '#61dafb' }) => {
+const Modal: FC<Modal_Props> = ({ header, content, footer }) => {
   return (
     <div className={styles.modal}>
       <div className={styles.modalDialog}>
-        <div className={styles.modalHeader} style={{ background: headerColor }}>
-          {header}
-        </div>
+        {header}
         <div className={styles.modalBody}>
           <div className={styles.modalContent}>{content}</div>
         </div>
