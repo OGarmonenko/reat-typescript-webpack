5d58d7b359b15a72a25c9672d3759151e3507407
Olga Garmonenko <olga.garmonenko@intexsoft.by>
2022-05-11 13:29:56 +0300

    Changed storeService

--- src/store/storeService.test.tsx
+++ src/store/storeService.test.tsx
@@ -14,17 +14,6 @@ describe('Test storeService', () => {
     expect(storeService.findRecord(mockID, mockState)).toEqual({ id: 2, item: 'test2', date: 1649918819350 });
   });
 
-  ///&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
-  test('push', () => {
-    const mockData = [{ id: 1, item: 'test1', date: 1649248316946 }];
-    storeService.pushRecords(mockData);
-  });
-
-  /*   test('rrr', () => {
-          const push = jest.spyOn(storeService, 'pushRecords');
-          push.mockReturnValue(mockState.records)
-      })*/
-
   afterEach(() => {
     jest.clearAllMocks();
   });
